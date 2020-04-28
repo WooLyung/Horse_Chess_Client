5eaa0cd86ee4f86a1dee1d226714475c07623827
WooLyung Lee <vlfkdydtk@naver.com>
2020-04-28 23:05:58 +0900

    add title scene

---
+++ Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    public Animation animation;
+    public bool isChange = false;
+
+	void Update ()
+    {
+        InputCheck();
+
+        if (!animation.IsPlaying("TitleAnim") && isChange)
+        {
+            ChangeScene();
+        }
+	}
+
+    private void InputCheck()
+    {
+        // 로그인 넘어가기
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!isChange)
+            {
+                animation.Play();
+                isChange = true;
+            }
+        }
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Login");
+    }
+}
