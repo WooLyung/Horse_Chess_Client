4295353d31f736f0cb4e1a7ead06afb9d33540cd
WooLyung Lee <vlfkdydtk@naver.com>
2020-05-01 00:33:06 +0900

    revise anims

---
+++ Assets/Scripts/LoginScene/BlurScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlurScript : MonoBehaviour
+{
+    [SerializeField]
+    public int _radius;
+    public Material blurMaterial;
+
+    public int radius
+    {
+        get
+        {
+            _radius = blurMaterial.GetInt("Radius");
+            return _radius;
+        }
+
+        set
+        {
+            Debug.Log("TEST");
+            blurMaterial.SetInt("Radius", value);
+        }
+    }
+}
