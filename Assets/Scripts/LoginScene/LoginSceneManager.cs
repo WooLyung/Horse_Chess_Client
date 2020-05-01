25b942e1f1d9b4f63eeb44397a42290f4a359b23
WooLyung Lee <vlfkdydtk@naver.com>
2020-05-01 21:10:04 +0900

    finish login scene design

---
+++ Assets/Scripts/LoginScene/LoginSceneManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginSceneManager : MonoBehaviour
+{
+    public Animator animator;
+    public LoginManager loginManager;
+
+    private int State
+    {
+        get
+        {
+            return animator.GetInteger("State");
+        }
+
+        set
+        {
+            animator.SetInteger("State", value);
+        }
+    }
+
+    private bool Text
+    {
+        get
+        {
+            return animator.GetBool("Text");
+        }
+
+        set
+        {
+            animator.SetBool("Text", value);
+        }
+    }
+
+    public void ToLogin()
+    {
+        if (Text == false && State == 1)
+        {
+            State = 0;
+        }
+    }
+
+    public void ToRegister()
+    {
+        if (Text == false && State == 0)
+        {
+            State = 1;
+        }
+    }
+}
