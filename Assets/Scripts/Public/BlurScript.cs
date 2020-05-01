9465254c0c451e64e9d995602e800c46cbd09b18
WooLyung Lee <vlfkdydtk@naver.com>
2020-05-01 17:02:39 +0900

    add login anims

---
+++ Assets/Scripts/Public/BlurScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlurScript : MonoBehaviour
+{
+    private int lastRadius = 0;
+
+    public Material blurMaterial;
+    public int radius = 0;
+
+    public void Update()
+    {
+        if (radius != lastRadius)
+        {
+            blurMaterial.SetInt("_Radius", radius);
+            lastRadius = radius;
+        }
+    }
+}
