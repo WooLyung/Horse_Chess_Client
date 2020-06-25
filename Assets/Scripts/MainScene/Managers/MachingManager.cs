c80940ab2160a91a730fb472bc8662a30f167eb0
WooLyung Lee <vlfkdydtk@naver.com>
2020-06-25 21:06:04 +0900

    add maching manager

--- Assets/Scripts/MainScene/Managers/MachingManager.cs
+++ Assets/Scripts/MainScene/Managers/MachingManager.cs
@@ -14,6 +14,8 @@ public class MachingManager : MonoBehaviour
     public Animator text2;
     public Animator loading1;
     public Animator loading2;
+    public Animator canvas;
+    public Text message;
     public Text button;
 
     private STATE state_;
@@ -57,6 +59,46 @@ public class MachingManager : MonoBehaviour
         text2.SetBool("isLoading", false);
         loading1.SetBool("isLoading", false);
         loading2.SetBool("isLoading", false);
-        button.text = "매칭 취소";
+        button.text = "게임 시작";
+    }
+
+    private void MachingFail()
+    {
+        state_ = STATE.NONE;
+
+        text1.SetBool("isLoading", false);
+        text2.SetBool("isLoading", false);
+        loading1.SetBool("isLoading", false);
+        loading2.SetBool("isLoading", false);
+        canvas.SetBool("Text", true);
+        message.text = "매칭에 실패했습니다";
+        button.text = "게임 시작";
+
+        StartCoroutine("MachingFailEnd");
+    }
+
+    private void MachingSuccess()
+    {
+        state_ = STATE.START;
+
+        text1.SetBool("isLoading", false);
+        text2.SetBool("isLoading", false);
+        loading1.SetBool("isLoading", false);
+        loading2.SetBool("isLoading", false);
+        canvas.SetInteger("State", 1);
+
+        StartCoroutine("ChangeScene");
+    }
+
+    IEnumerator MachingFailEnd()
+    {
+        yield return new WaitForSeconds(1.5f);
+        canvas.SetBool("Text", false);
+    }
+
+    IEnumerator ChangeScene()
+    {
+        yield return new WaitForSeconds(1f);
+        Debug.Log("씬 전환");
     }
 }
