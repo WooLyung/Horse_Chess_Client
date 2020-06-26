e64a0aed3bbfa56c96af6cb09766722e7cb7dd90
WooLyung Lee <vlfkdydtk@naver.com>
2020-06-26 12:08:29 +0900

    fix width

---
+++ Assets/Scripts/InGameScene/FixedCamera.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCamera : MonoBehaviour
+{
+	void Awake ()
+    {
+        Camera.main.orthographicSize = 9 * 0.5625f / Camera.main.aspect;
+    }
+}
